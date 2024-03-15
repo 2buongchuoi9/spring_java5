45918043c614bc8aef2ff41db9bfd6e6fe4ec97e
dungnguyen <nguyenssk043@gmail.com>
2024-03-15 19:51:22 +0700

    test cicd 17

--- src/test/java/daden/shopaa/SpringJava5ApplicationTests.java
+++ src/test/java/daden/shopaa/SpringJava5ApplicationTests.java
@@ -1,8 +1,14 @@
 package daden.shopaa;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJava5ApplicationTests {
+
+  @Test
+  public void testtt() {
+    System.out.println("test_Yes");
+  }
 
 }
